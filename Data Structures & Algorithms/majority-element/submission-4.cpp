c906eae23e6328701cbfdaa0dd09f54bf7758490
tanzeel123raza-byte <tanzeel123raza@gmail.com>
2026-08-18 21:02:15 +0530

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for(int i : nums)
+        {
+            mp[i]++;
+
+            if(mp[i] > nums.size()/2)
+                return i;
+        }
+        return -1;
+    }
+};
