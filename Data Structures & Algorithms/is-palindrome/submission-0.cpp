78e1d4c16309849046a967414c5a22380a1207ab
tanzeel123raza-byte <tanzeel123raza@gmail.com>
2026-08-21 06:41:08 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp = "";
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(isalnum(s[i]))
+            {
+                temp += tolower(s[i]);
+            }
+        }
+        int i = 0;
+        int j = temp.size()-1;
+
+        while(i <= j)
+        {
+            if(temp[i] != temp[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
