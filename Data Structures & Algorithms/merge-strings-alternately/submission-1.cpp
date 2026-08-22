b28c6a44e2672777310d2447dd1e5c5f7f8338e7
tanzeel123raza-byte <tanzeel123raza@gmail.com>
2026-08-22 10:51:30 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string temp = "";
+        int i = 0;
+        int j = 0;
+
+        while(i < word1.size() || j < word2.size())
+        {
+            if(i < word1.size()) temp += word1[i++];
+            if(j < word2.size()) temp += word2[j++];
+        }
+        return temp;
+    }
+};
